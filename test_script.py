0dc351fbfaacd7dd6259fdc636b2d9a77e63e761
Lynn-LichaaElKhoury <lynn.lichaaelkhoury@gmail.com>
2023-03-22 12:42:24 +0200

    added args

--- test_script.py
+++ test_script.py
@@ -1,3 +1,7 @@
 import re
 import argparse
 import requests
+
+args = argparse.ArgumentParser()
+args.add_argument("domainName", help="'domain name")
+domain=getattr(args.parse_args(), "domainName")
