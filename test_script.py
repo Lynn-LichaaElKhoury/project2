5e8d6f2138fc723b98cef3b0ff27361a9b57cc0c
Lynn-LichaaElKhoury <lynn.lichaaelkhoury@gmail.com>
2023-03-22 12:59:34 +0200

    validation

--- test_script.py
+++ test_script.py
@@ -5,3 +5,13 @@ import requests
 args = argparse.ArgumentParser()
 args.add_argument("domainName", help="'domain name")
 domain=getattr(args.parse_args(), "domainName")
+
+
+if not re.match(r"^https?:\/\/", domain):
+    secure = input("protocol secure ? (yes or no)")
+    if secure.lower() == "yes" :
+        domain = "https://"+domain
+    elif secure.lower() == "no":
+        domain = "http://"+domain
+    
+
